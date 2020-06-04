30a03d9860f3d4d02b365252c6473ddbc1bfbfe0
StrangeWorldCuteMe <34224030+PEIXingting@users.noreply.github.com>
2020-06-05 02:03:42 +0800

    Update commonly_used_examples.go

--- Learning Codes/commonly_used_examples.go
+++ Learning Codes/commonly_used_examples.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 )
 
-//random digit
 func main() {
+	// random digit
 	t1 := time.Now()
 	timeStamp1 := t1.Unix()
 	fmt.Println(timeStamp1)
@@ -17,4 +18,21 @@ func main() {
 	fmt.Println(rand.Intn(100))
 	num1 := rand.Intn(10) + 3 // [3,12]
 	fmt.Println(num1)
+
+	// get ordered key, value from map
+	map1 := make(map[int]string)
+	map1[1] = "one"
+	map1[2] = "two"
+	map1[3] = "three"
+	map1[4] = "four"
+	map1[5] = "five"
+
+	keys := make([]int, 0, len(map1))
+	for k, _ := range map1 {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, key := range keys {
+		fmt.Println(key, map1[key])
+	}
 }
